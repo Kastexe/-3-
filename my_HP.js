508815d6a5db790113b56c310ce71e0cbd6a2b02
Kastexe <136457939+Kastexe@users.noreply.github.com>
2024-07-19 12:17:43 +0900

    Add files via upload

---
+++ my_HP.js
@@ -0,0 +1,11 @@
+//さよならボタンの動作
+$("#bye").on("click",function(){
+    alert("このメッセージ消すとどっかに左遷するお☆");
+    window.location.href = 'https://google.com'; // 通常の遷移
+    //window.open('パス名', '_blank'); // 新しいタブを開き、ページを表示
+});
+//ひまボタンの動作
+$("#hima").on("click",function(){
+    alert("ひまやなぁ....せや!こことかどうや？(タブ開くで)")
+    window.open("https://x.com","_blank")
+});
