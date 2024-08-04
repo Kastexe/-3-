13bf9029601b14eaaea73a9d5a150327619565d6
Kastexe <136457939+Kastexe@users.noreply.github.com>
2024-08-05 00:26:20 +0900

    Add files via upload

--- my_HP.js
+++ my_HP.js
@@ -6,11 +6,26 @@ $("#bye").on("click",function(){
 });
 //ひまボタンの動作
 $("#hima").on("click",function(){
-    alert("ひまやなぁ....せや!こことかどうや？(タブ開くで)")
-    window.open("https://x.com","_blank")
+    //window.confirmでOK,キャンセルの2択にできる
+        if (window.confirm("ひまやなぁ....せや!こことかどうや？(タブ開くで)")){
+            alert("ほいほい☆")
+            window.open("https://x.com","_blank")
+        }else{
+            alert("自分で探そか(((鬼畜")
+            window.open("https://google.com","_blank")
+        }
 });
 //神様参拝ボタン
 $("#god").on("click",function(){
     alert("おっ(^ω^)いいことだ☆")
-    window.open("https://twitter.com/Yumely21")
+    if(window.confirm("今ひま？")){
+        window.open("https://twitter.com/Yumely21","_blank")
+    }else{
+        window.open("https://x.com/yukkuri_light")
+    }
+});
+//なにここボタン
+$("#wh").on("click",function(){
+    alert("しらねーよ")
+    alert("なんもないただのHPだよ")
 });
